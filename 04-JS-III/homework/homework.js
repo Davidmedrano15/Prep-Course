8ab31d653043b3d6ae6e8568fc729ef414880dd9
´´DavidMedrano <davidmedranoh@hotmail.com``>
2022-07-16 10:48:07 -0500

    finish homework 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,7 +3,7 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
-  return [0]
+  return [0];
 }
 
 
@@ -26,10 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-  var array = []
-  for ( var i = 0; i < array.length; i++) {
-    array [i] +1;
+  var array1 = [] 
+  for ( var i= 0; i < array.length; i++) 
+    array1 [i] = array [i] + 1;
   }
+  return array1;
 }
 
 
@@ -58,7 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  return ["true", "palabra", "2"]
+  return palabras.unshift('')
 }
 
 
@@ -67,9 +68,9 @@ function arrayContiene(array, elemento) {
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
   for(var i = 0; i < array.length; i++) {
-    if ( array [i] === elemento) 
-      return true
-  }
+    if ( array [i] === elemento) {
+      return true;
+    }
 }
 return false
 }
@@ -81,7 +82,7 @@ function agregarNumeros(numeros) {
   // Tu código:
   var suma = 0;
   for (var i = 0; i < numeros.length; i++) {
-    suma = 0 + numeros [i]
+    suma = suma + numeros [i];
   }
   return suma;
 }
@@ -91,7 +92,8 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-     return promedioResultadosTest[resultadosTest] / resultadosTest.length;
+     return promedioResultadosTest(resultadosTest) 
+     resultadosTest.length;
 
 }
 
@@ -103,6 +105,7 @@ function numeroMasGrande(numeros) {
   var grande = numeros [0];
   for (var i = 0; i < numeros.length; i++)
     if(numeros [i] > grande) {
+      grande = numeros [i];
     }
 }
     return grande;
@@ -127,8 +130,8 @@ function cuentoElementos(arreglo){
   //Escribe tu código aquí
   var cantidad = 0 ;
   for (var i = 0 ; i < array.length; i++) {
-    if (array [i] > 19) {
-      cantidad++
+    if (array [i] > 18) {
+      i++
     }
   }
        return cantidad;
@@ -152,7 +155,7 @@ function empiezaConNueve(n) {
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
   var num = "9"
-  if (num . charat (0) === "9") {
+  if (num.charat (0) === "9") {
     return true
   }
   return false
